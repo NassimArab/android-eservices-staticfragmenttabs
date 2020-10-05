c51d2937a072f722e600edf0304ab0f9f0c42e78
arab <nassimarab@gmail.com>
2020-10-05 03:29:00 +0200

    Partie 2 terminée

--- app/src/main/java/android/eservices/staticfragmenttabs/FragmentOne.java
+++ app/src/main/java/android/eservices/staticfragmenttabs/FragmentOne.java
@@ -1,15 +1,33 @@
 package android.eservices.staticfragmenttabs;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 //TODO : fix this fragment so it works :)
 //Once it's done, then create a second fragment with the other layout
-public class FragmentOne extends Fragment {
+public class FragmentOne extends Fragment  implements View.OnClickListener {
+
+    //2 - Declare callback
+    private OnButtonClickedListener mCallback;
+
+    @Override
+    public void onClick(View view) {
+        // 5 - Spread the click to the parent activity
+        mCallback.onButtonClicked(view);
+    }
+
+    // 1 - Declare our interface that will be implemented by any container activity
+    public interface OnButtonClickedListener {
+        public void onButtonClicked(View view);
+    }
+
+    // ----
 
     public static final String TAB_NAME = "ADD TO COUNTER";
 
@@ -28,6 +46,7 @@ public class FragmentOne extends Fragment {
         ViewGroup fragmentOneView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_one, container, false);
 
+    fragmentOneView.findViewById(R.id.button_increment).setOnClickListener(this);
         return fragmentOneView;
     }
 
@@ -40,6 +59,26 @@ public class FragmentOne extends Fragment {
     public void onStart() {
         super.onStart();
     }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // 4 - Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
+    }
+
+    // 3 - Create callback to parent activity
+    private void createCallbackToParentActivity(){
+        try {
+            //Parent activity will automatically subscribe to callback
+            mCallback = (OnButtonClickedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(e.toString()+ " must implement OnButtonClickedListener");
+        }
+    }
+
 
     //TODO add listener to button and transmit the information to parent Activity
     //TODO read the Android doc, as suggested, to do it the right way
--- app/src/main/java/android/eservices/staticfragmenttabs/FragmentTwo.java
+++ app/src/main/java/android/eservices/staticfragmenttabs/FragmentTwo.java
@@ -1,14 +1,28 @@
 package android.eservices.staticfragmenttabs;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
-public class FragmentTwo extends Fragment {
+public class FragmentTwo extends Fragment  implements View.OnClickListener {
 
-    public static final String TAB_NAME = "ADD TO COUNTER";
+    //2 - Declare callback
+    private FragmentOne.OnButtonClickedListener mCallback;
+
+    @Override
+    public void onClick(View view) {
+        // 5 - Spread the click to the parent activity
+        mCallback.onButtonClicked(view);
+    }
+
+    // ----
+    private Button moins;
+
+    public static final String TAB_NAME = "SUB TO COUNTER";
 
     public FragmentTwo() {
         //TODO
@@ -24,6 +38,7 @@ public class FragmentTwo extends Fragment {
                              Bundle savedInstanceState) {
         ViewGroup fragmentOneView = (ViewGroup) inflater.inflate(
                 R.layout.fragment_two, container, false);
+        fragmentOneView.findViewById(R.id.button_decrement).setOnClickListener(this);
 
         return fragmentOneView;
     }
@@ -37,6 +52,27 @@ public class FragmentTwo extends Fragment {
     public void onStart() {
         super.onStart();
     }
+
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        // 4 - Call the method that creating callback after being attached to parent activity
+        this.createCallbackToParentActivity();
+    }
+
+    // 3 - Create callback to parent activity
+    private void createCallbackToParentActivity(){
+        try {
+            //Parent activity will automatically subscribe to callback
+            mCallback = (FragmentOne.OnButtonClickedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(e.toString()+ " must implement OnButtonClickedListener");
+        }
+    }
+
 
     //TODO add listener to button and transmit the information to parent Activity
     //TODO read the Android doc, as suggested, to do it the right way
